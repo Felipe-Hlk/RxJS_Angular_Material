25ecf8ba0b50c52c29779a13521e19438a50f1b6
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-07-10 22:38:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import {from} from "rxjs";
 
 let numbers = [1, 5, 10];
-let source = from (numbers);
+let source = from (numbers); //elemento observable
 
 function component() {
-    source.subscribe({
+    source.subscribe({ //utilizando do subscribe para poder executar o observable source
         next: (x) => console.log(x)
     })
     
