ade5b6404d8906d5f66f3c99a0596aaae168ab6d
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-07-17 12:07:56 -0300

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
-import {from} from "rxjs";
+import {filter, from, map, Observable} from "rxjs";
 
-let numbers = [1, 5, 10];
-let source = from (numbers); //elemento observable
+let numbers  = [1,5,10,20,30,40,50,60,70,80,90,100];
 
-function component() {
-    source.subscribe({ //utilizando do subscribe para poder executar o observable source
-        next: (x) => console.log(x)
-    })
+let source = new Observable(subscriber => {
+    let index = 0;
+    let produceValue = () => {
+        subscriber.next(numbers[index++])
+        if (index < numbers.length) {
+            setTimeout(produceValue, 500);
+        }else{
+            subscriber.complete();
+        }
+    }
     
-  }
+    produceValue();
 
-(component());
+})
+
+source.pipe(
+    map((n: number) => n * 2)
+).subscribe({
+    next: (x:number) =>{
+        console.log(x)
+    },
+    error: (e: Error) => console.log(e),
+    complete: () => console.log ('Complete'),
+})
+
+source.pipe(
+    filter((n: number) => n > 5)
+).subscribe({
+    next: (x:number) =>{
+        console.log(x)
+    },
+    error: (e: Error) => console.log(e),
+    complete: () => console.log ('Complete'),
+})
