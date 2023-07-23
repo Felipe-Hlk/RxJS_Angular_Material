0502162314dc92c98db76970def1ef5e66df691d
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-07-23 17:59:39 -0300

    update components

--- RxJS_SImple_TS/Angular-Material/src/app/app.component.ts
+++ RxJS_SImple_TS/Angular-Material/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core'
+import {BreakpointObserver} from '@angular/cdk/layout'
 import { fromEvent, map } from 'rxjs';
+
 
 export const SCROLL_CONTAINER = 'mat-sidenav-content';
 export const TEXT_LIMIT = 50;
 export const SHADOW_LIMIT = 100;
+
 
 @Component({
   selector: 'app-root',
@@ -12,37 +14,47 @@ export const SHADOW_LIMIT = 100;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public isSmallScreen = false;
+
+  public smallScreen = false;
   public popText = false;
   public applyShadow = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
+  constructor(private breakpointObserver: BreakpointObserver){ }
   ngOnInit(): void {
     const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+    console.log (content);
 
-    fromEvent(content, 'scroll')
-      .pipe(
-        map(() => content.scrollTop)
-      )
-      .subscribe({
-        next: (value: number) => this.determineHeader(value)
-      })
+    fromEvent (content, 'scroll')
+    .pipe(
+      map(() => content.scrollTop)
+    )
+    .subscribe({
+      next: (value: number) => this.determineHeader(value)
+    })
   }
 
-  determineHeader(scrollTop: number) {
-    console.log(scrollTop)
-    this.popText = scrollTop >= TEXT_LIMIT;
-    this.applyShadow = scrollTop >= SHADOW_LIMIT;
+  determineHeader (scrollHeight: number){
+    console.log(scrollHeight)
+    this.popText = scrollHeight >= TEXT_LIMIT;
+    this.applyShadow = scrollHeight >= SHADOW_LIMIT;
   }
 
-  ngAfterContentInit(): void {
-    this.breakpointObserver
-        .observe(['(max-width: 800px)'])
-        .subscribe((res) => this.isSmallScreen = res.matches);
+
+
+  ngAfterContentInit(): void{
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+      next: (res) => {                 //res = retorno
+        if (res.matches){
+          this.smallScreen = true;
+        }else{
+          this.smallScreen = false;
+        }
+      }
+    })
   }
 
   get sidenavMode() {
-    return this.isSmallScreen ? 'over' : 'side';
+    return this.smallScreen ? 'over' : 'side';
   }
+
 }
