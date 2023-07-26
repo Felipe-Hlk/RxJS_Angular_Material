22a4bb10526401efaff135c37afc21b7b15535f0
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-07-25 21:47:32 -0300

    ProgressBar com RxJS

--- RxJS_SImple_TS/Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
+++ RxJS_SImple_TS/Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgressBarMode } from '@angular/material/progress-bar';
+import { concat, interval, map, take, takeWhile, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-progress-spinner',
@@ -7,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressSpinnerComponent implements OnInit {
 
-  public loadingPercent = 50;
+  public loadingPercent = 0;
+  public queryValue = 0;
+  public queryMode: ProgressBarMode = 'query';
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.loadingProgress(500)
+    .subscribe(i => this.loadingPercent = i);
+
+    concat(
+      interval(2000)
+        .pipe(
+          take(1),
+          tap(_ => (this.queryMode = 'determinate'))
+        ),
+      this.loadingProgress(500)
+    ).subscribe(i => this.queryValue = i);
+
+  }
+
+  loadingProgress(speed: number){
+    return interval(speed)
+    .pipe(
+      map( i => i * 10),
+      takeWhile( i => i <= 100)
+    )
   }
 
 }
