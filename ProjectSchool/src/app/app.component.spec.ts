fdd3ad5c864c1ebb60c4b560c650c14b7f151aaa
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-07 15:01:09 -0300

    create new project

--- ProjectSchool/src/app/app.component.spec.ts
+++ ProjectSchool/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProjectSchool'`, () => {
+  it(`should have as title 'projectSchool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProjectSchool');
+    expect(app.title).toEqual('projectSchool');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjectSchool app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projectSchool app is running!');
   });
 });
--- ProjectSchool/src/app/app.module.ts
+++ ProjectSchool/src/app/app.module.ts
@@ -1,61 +1,18 @@
-import {NgModule} from '@angular/core';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatTableModule} from '@angular/material/table';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-
-  exports: [
-    MatSlideToggleModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatInputModule,
-    MatListModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatTabsModule,
-    MatTableModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatMomentDateModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatPaginatorModule,
-    MatSortModule
-
+  declarations: [
+    AppComponent
   ],
-  declarations:[],
-  providers:[
-    {
-      provide: MAT_DATE_LOCALE, useValue: 'pt-br'
-    },
-    {
-      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue:
-      {
-        horizontalPosition:'end',
-        verticalPosition: 'bottom',
-        duration: 2500
-      }
-    }
-  ]
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-export class MaterialModule { }
+export class AppModule { }
